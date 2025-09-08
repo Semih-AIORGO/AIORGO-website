b35ee1fb47d737b8cceda0cfaf11fcc0fc1b3a0f
Semih <semih@aiorgo.com>
2025-09-08 10:19:49 +0300

    Complete website rebuild: Exact clone of Alex's site with AIORGO branding

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Simple build script for static site deployment
-echo "Building static site..."
-echo "No build process required for static HTML/CSS/JS files"
-echo "Site is ready for deployment!"
